0b9417e470f185b4df299f44697c1acff5f233cc
Clive Lee <clive.hg.lee@gmail.com>
2023-04-10 00:00:05 -0400

    second date type added

--- htmlparser.py
+++ htmlparser.py
@@ -35,11 +35,9 @@ def update_image_date_taken(image_path, new_date):
 
 
 month_names = r'(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)[a-z]*'
-# first_pattern = rf'\b{month_names}\s+\d{{1,2}}(?:st|nd|rd|th)?(?:,\s+\d{{2,4}})?\b'
 first_pattern = rf'\b({month_names})\s+(\d{{1,2}})(?:st|nd|rd|th)?(?:,\s+(\d{{2,4}}))?\b'
 
 def try_first_pattern(text, prev_date=None):
-    
     date_match = re.search(first_pattern, text, flags=re.IGNORECASE)
     
     if not date_match:
@@ -63,7 +61,7 @@ def try_first_pattern(text, prev_date=None):
     # Match year
     if year:
         year = int(year)
-        if len(str(year)) == 2:
+        if len(str(year)) <= 2:
             year += 2000
 
     # Set the year if missing
@@ -84,22 +82,63 @@ def try_first_pattern(text, prev_date=None):
     # Return the date as a datetime instance
     return datetime(year, month, day)
 
+
+
 date_patterns = [
-    # Format: "January 26th", "Feb 3rd", "March 5"
-    rf'\b{month_names}\s+\d{{1,2}}(?:st|nd|rd|th)?(?:,\s+\d{{2,4}})?\b',
-
-    # Format: "26 January", "3 Feb", "5 March"
-    rf'\b\d{{1,2}}(?:st|nd|rd|th)?\s+{month_names}(?:\s+\d{{2,4}})?\b',
-
-    # Format: "01-26-2022", "1-26-22", "01/26/2022", "1/26/22", "01\26\2022", "1\26\22"
-    r'\b\d{1,2}[-/\\]\d{1,2}[-/\\]\d{2,4}\b'
+    # Format: "01-26-2022", "1-26-22", "01/26/2022", "1/26/22", "01\26\2022", "1\26\22
 ]
+second_pattern = r'\b(\d{1,2})[-\/\\](\d{1,2})[-\/\\](?:(\d{2,4}))\b'
+
+def try_second_pattern(text, prev_date=None):
+    date_match = re.search(second_pattern, text, flags=re.IGNORECASE)
+    
+    if not date_match:
+        return None
+    try:
+        month, day, year = date_match.group(1,2,3)
+    except IndexError:
+        month, day = date_match.group(1,2)
+        year = None
+    
+    # Match month
+    month = int(month)
+
+    # Match day
+    day = int(day)
+
+    # Match year
+    if year:
+        year = int(year)
+        if len(str(year)) <= 2:
+            year += 2000
+
+    # Set the year if missing
+    if year is None:
+        if prev_date:
+            prev_year = prev_date.year
+            test_date = datetime(prev_year, month, day)
+            if test_date > prev_date:
+                year = prev_year - 1
+            else:
+                year = prev_year
+        else:
+            year = 2023
+            test_date = datetime(year, month, day)
+            if test_date > datetime.now():
+                year -= 1
+    # Return the date as a datetime instance
+    return datetime(year, month, day)
+
 
 def parse_date_from_text(text, prev_date=None):
     if text is None:
         return None
     
     parsed_datetime = try_first_pattern(text, prev_date)
+    if parsed_datetime:
+        return parsed_datetime
+    
+    parsed_datetime = try_second_pattern(text, prev_date)
 
     if parsed_datetime:
         return parsed_datetime
@@ -127,7 +166,7 @@ def process_html(html_file, end_date=None, current_date=datetime.today()):
         if element.name == 'span':
             date = parse_date_from_text(element.string)
             if date:
-                print(f"date parsed: {date} from date_string {element.string}")
+                # print(f"date parsed: {date} from date_string {element.string}")
                 previous_date_year = date.year
                 datesParsed += 1
                 if end_date and current_date <= end_date:
